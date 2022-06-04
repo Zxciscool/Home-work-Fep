594580fe8949606d0e28881e7f1f199fa3ddb072
Zxciscool <maksym.golubov@gmail.com>
2022-06-04 13:56:34 +0300

    hot fix

--- golubovHw13/script.js
+++ golubovHw13/script.js
@@ -1,12 +1,12 @@
-const btnEi = document.getElementById("butt")
-const inpEi = document.getElementById("text")
-const strEi = document.getElementById("string")
+const btnEi = document.getElementById("butt");
+const inpEi = document.getElementById("text");
+const strEi = document.getElementById("string");
 let val;
 
 btnEi.onclick = () => {
     val = inpEi.value;
-    btnEi.classList.toggle("hidden")
-    inpEi.classList.toggle("hidden")
+    btnEi.classList.toggle("hidden");
+    inpEi.classList.toggle("hidden");
     strEi.innerHTML = `Привет, ${val}`;
 
 }
